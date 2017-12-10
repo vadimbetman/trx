03a274ad1c254fb5611fe6480f2b1ea8f93768e3
vadimbetman <vadimbetman@i.ua>
2017-12-10 06:31:01 +0200

    file is added

---
+++ copy.sh
@@ -0,0 +1,8 @@
+cp apps/compal_dsp_dump/main.c $PWD/../trx/src/target/firmware/apps/compal_dsp_dump/main.c &&
+cp apps/hello_world/main.c $PWD/../trx/src/target/firmware/apps/hello_world/main.c &&
+cp apps/layer1/main.c $PWD/../trx/src/target/firmware/apps/layer1/main.c &&
+cp apps/loader/main.c $PWD/../trx/src/target/firmware/apps/loader/main.c &&
+cp apps/loader_mtk/main.c $PWD/../trx/src/target/firmware/apps/loader_mtk/main.c &&
+cp apps/rssi/main.c $PWD/../trx/src/target/firmware/apps/rssi/main.c &&
+cp apps/simtest/main.c $PWD/../trx/src/target/firmware/apps/simtest/main.c &&
+cp apps/trx/main.c $PWD/../trx/src/target/firmware/apps/trx/main.c
